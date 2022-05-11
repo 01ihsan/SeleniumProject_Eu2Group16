1983ed1be1537dc1f379c47ce88479eefa52c526
tolga <tolgagunes87@gmail.com>
2022-05-11 12:37:25 +0200

    Refresh button was re-added.

--- src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
@@ -13,6 +13,7 @@ public class IhsanSeleniumTest {
         driver.get("https://www.google.com");
         WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
         agreeButton.click();
+        driver.navigate().refresh();
 
     }
 }
