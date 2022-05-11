53401b06d91ae1a9b856258d7596dee944b9f582
01ihsan <karatasihsan01@gmail.com>
2022-05-11 10:16:46 +0200

    IhsanSeleniumTest file created.

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.test.gitHub.practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IhsanSeleniumTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
+        agreeButton.click();
+    }
+}
