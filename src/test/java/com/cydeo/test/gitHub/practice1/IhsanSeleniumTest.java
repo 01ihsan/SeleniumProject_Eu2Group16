78f92bd164e718c439dc347a612888d682a4828f
EnesJildirim <enesysaid@gmail.com>
2022-05-12 19:35:41 +0200

    Comment

--- src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
@@ -17,5 +17,7 @@ public class IhsanSeleniumTest {
         driver.get("https://www.google.com");
         WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
         agreeButton.click(); //click button
+
+        //Hello
     }
 }
