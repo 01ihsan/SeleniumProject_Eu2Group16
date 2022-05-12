26fd3af713ce71520b69de6aafb82ebe507a03df
01ihsan <karatasihsan01@gmail.com>
2022-05-12 11:59:27 +0200

    maximize and wait method added

--- src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/IhsanSeleniumTest.java
@@ -6,10 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class IhsanSeleniumTest {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // Wait up to 5 seconds if there is a problem. Day4 Selenium Class.
         driver.get("https://www.google.com");
         WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
         agreeButton.click(); //click button
