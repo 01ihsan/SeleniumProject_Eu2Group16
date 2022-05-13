8bae0522bd2ea0ed4b301e6d016ec2958e02c2d3
01ihsan <karatasihsan01@gmail.com>
2022-05-13 14:17:35 +0200

    Rename file

--- src/test/java/com/cydeo/test/gitHub/practice1/TestNGIntro.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/TestNGIntro.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TestNG_Intro {
+public class TestNGIntro {
     @AfterMethod
     public void tearDownMethod(){
         System.out.println("method is over");
