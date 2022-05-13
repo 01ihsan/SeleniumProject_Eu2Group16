12e9210d5497d1b4976f3b0bbce586aa78e5b211
01ihsan <karatasihsan01@gmail.com>
2022-05-13 14:48:43 +0200

    T4_SimpleDropdowns created

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/T4_SimpleDropdowns.java
@@ -0,0 +1,31 @@
+package com.cydeo.test.gitHub.practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("NewClassNamingConvention")
+public class T4_SimpleDropdowns {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+
+    @Test
+    public void simpleDropdownTest() {
+        Select dateDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+    }
+}
