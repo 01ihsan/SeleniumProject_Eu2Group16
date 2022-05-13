359864cfeaa7e84b65fa7e27aea1b673768816f9
01ihsan <karatasihsan01@gmail.com>
2022-05-13 13:56:39 +0200

    TestNG setup completed

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/TestNG_Intro.java
@@ -0,0 +1,28 @@
+package com.cydeo.test.gitHub.practice1;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("method is over");
+        System.out.println();
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        System.out.println("method is initializing");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("test1 run..");
+    }
+    @Test
+    public void test2(){
+        System.out.println("test2 run..");
+    }
+}
