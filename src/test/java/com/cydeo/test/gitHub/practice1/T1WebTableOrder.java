d0c088c46920bd4178c5554da1dee66a94e7a172
01ihsan <karatasihsan01@gmail.com>
2022-05-30 13:48:19 +0200

    T1WebTableOrder Created

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/T1WebTableOrder.java
@@ -0,0 +1,26 @@
+package com.cydeo.test.gitHub.practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1WebTableOrder {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
+    @Test
+    public void nameVerify_test(){
+
+    }
+}
