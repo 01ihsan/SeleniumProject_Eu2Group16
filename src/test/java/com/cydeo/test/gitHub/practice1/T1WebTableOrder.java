675a146ba4d7378ac41747ad6a975f0f1373f32d
01ihsan <karatasihsan01@gmail.com>
2022-05-30 14:06:51 +0200

    T1WebTableOrder done

--- src/test/java/com/cydeo/test/gitHub/practice1/T1WebTableOrder.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/T1WebTableOrder.java
@@ -1,8 +1,11 @@
 package com.cydeo.test.gitHub.practice1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -21,6 +24,13 @@ public class T1WebTableOrder {
     }
     @Test
     public void nameVerify_test(){
-
+        WebElement bobMartinCell =
+                driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
+        System.out.println("Bob Martin" +" - "+bobMartinCell.getText());
+        Assert.assertEquals("Bob Martin",bobMartinCell.getText());
+        WebElement bobMartinDate =
+                driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
+        Assert.assertEquals("12/31/2021",bobMartinDate.getText());
+        System.out.println("12/31/2021" +" - "+ bobMartinDate.getText());
     }
 }
