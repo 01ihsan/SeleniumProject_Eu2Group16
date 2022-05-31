0c38c2aabc8016f119eedc32162b2f07654d23e5
01ihsan <karatasihsan01@gmail.com>
2022-05-31 10:37:39 +0200

    ConfigurationPractice created

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/ConfigPractice.java
@@ -0,0 +1,28 @@
+package com.cydeo.test.gitHub.practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConfigPractice {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void google_search_test(){
+
+    }
+
+}
