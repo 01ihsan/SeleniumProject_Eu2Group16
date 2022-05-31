c5438f7fb1e73d0b9c468cfd9995140b787354fd
01ihsan <karatasihsan01@gmail.com>
2022-05-31 10:46:51 +0200

    ConfigurationPractice modified

--- src/test/java/com/cydeo/test/gitHub/practice1/ConfigPractice.java
+++ src/test/java/com/cydeo/test/gitHub/practice1/ConfigPractice.java
@@ -1,8 +1,12 @@
 package com.cydeo.test.gitHub.practice1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,10 +22,17 @@ public class ConfigPractice {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
     }
 
     @Test
     public void google_search_test(){
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='q']"));
+
+        searchBox.sendKeys("apple"+ Keys.ENTER);
+
+        String currentTitle = driver.getTitle();
+        Assert.assertEquals("apple - Google Search",currentTitle);
 
     }
 
