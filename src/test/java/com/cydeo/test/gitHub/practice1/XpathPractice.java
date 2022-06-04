57f37ac7457db90e94f92e781eb8ccb842ea808b
01ihsan <karatasihsan01@gmail.com>
2022-06-04 21:13:21 +0200

    XpathPractice Created

---
+++ src/test/java/com/cydeo/test/gitHub/practice1/XpathPractice.java
@@ -0,0 +1,13 @@
+package com.cydeo.test.gitHub.practice1;
+
+import com.cydeo.test.gitHub.utilities.Driver;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class XpathPractice {
+    @Test
+    public void automationExercise(){
+        Driver.getDriver().get("https://automationexercise.com/");
+        Driver.getDriver().findElement(By.xpath("//ul[@class='nav navbar-nav']//li[4]")).click();
+    }
+}
