4f1a68ed59d2e996e8074fcf00de976c0fd8a0ea
01ihsan <karatasihsan01@gmail.com>
2022-06-09 13:15:44 +0200

    POMPractices updated

--- src/test/java/com/cydeo/test/gitHub/practice3/POMPractices.java
+++ src/test/java/com/cydeo/test/gitHub/practice3/POMPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.test.gitHub.practice3;
 
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.test.gitHub.utilities.Driver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class POMPractices {
@@ -9,6 +10,10 @@ public class POMPractices {
     public void requiredFieldErrorMessageTest() {
         Driver.getDriver().get("https://library1.cydeo.com/login.html");
         LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
-
+        libraryLoginPage.signInButton.click();
+        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+        Driver.closeDriver();
     }
+    @Test
+    public void
 }
