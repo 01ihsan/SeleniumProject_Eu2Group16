dcee1054690c195ba934fe318a6d7b0553418c54
01ihsan <karatasihsan01@gmail.com>
2022-06-09 14:56:09 +0200

    Synchronizing practice

--- src/test/java/com/cydeo/test/gitHub/practice3/WebDriverWaitPractices.java
+++ src/test/java/com/cydeo/test/gitHub/practice3/WebDriverWaitPractices.java
@@ -5,6 +5,7 @@ import com.cydeo.test.gitHub.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WebDriverWaitPractices {
@@ -14,6 +15,6 @@ public class WebDriverWaitPractices {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
         wait.until(ExpectedConditions.titleIs("Dynamic title"));
         DynamicLoad7Page dynamicLoad7Page = new DynamicLoad7Page();
-
+        Assert.assertTrue(dynamicLoad7Page.doneMessage.isDisplayed());
     }
 }
