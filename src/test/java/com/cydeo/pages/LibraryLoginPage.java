1993cee4330336322464bad8af68f97da5e4def1
01ihsan <karatasihsan01@gmail.com>
2022-06-09 12:59:34 +0200

    LibraryLoginPage Updated

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.test.gitHub.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
@@ -8,5 +10,12 @@ public class LibraryLoginPage {
     public LibraryLoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement inputUserName;
+    @FindBy(xpath = "//input[@id='inputPassword']")
+    public WebElement inputPassword;
+    @FindBy(className = "btn btn-lg btn-primary btn-block")
+    public WebElement signIn;
+    @FindBy(xpath = "//div[@id='inputEmail-error']")
+    public WebElement errorMessage;
 }
