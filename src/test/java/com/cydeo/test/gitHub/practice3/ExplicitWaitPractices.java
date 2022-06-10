fc6604fc9d574b495d8dcc63638655205068821b
01ihsan <karatasihsan01@gmail.com>
2022-06-10 14:26:58 +0200

    implicit and explicit wait tuning

--- src/test/java/com/cydeo/test/gitHub/practice3/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/test/gitHub/practice3/ExplicitWaitPractices.java
@@ -1,6 +1,7 @@
 package com.cydeo.test.gitHub.practice3;
 
 import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.test.gitHub.utilities.BrowserUtils;
 import com.cydeo.test.gitHub.utilities.Driver;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.Actions;
@@ -10,22 +11,23 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class ExplicitWaitPractices {
     DynamicControlsPage dynamicControlsPage;
-    Actions actions;
-    WebDriverWait wait;
-
     @BeforeMethod
     public void setupMethod(){
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
         dynamicControlsPage = new DynamicControlsPage();
-        actions = new Actions(Driver.getDriver());
-        wait = new WebDriverWait(Driver.getDriver(), 10);
     }
     @Test
     public void removeButtonTest(){
         dynamicControlsPage.removeButton.click();
-        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+        Actions actions = new Actions(Driver.getDriver());
+//        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+//        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+//        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
         try{
             Assert.assertFalse(dynamicControlsPage.checkbox.isDisplayed());
         }catch (NoSuchElementException n){
